b5161f9bae755b2f3599abb153412211a5d682ae
K0966914 <yanasiri.s@kbtg.tech>
2025-08-27 12:07:54 +0700

    init front

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="content">
+        <h1>Hello World</h1>
+        <p>Welcome to React with Vite!</p>
+        <div className="decorative-elements">
+          <div className="circle circle-1"></div>
+          <div className="circle circle-2"></div>
+          <div className="circle circle-3"></div>
+        </div>
+        <button className="cta-button">
+          Get Started
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
